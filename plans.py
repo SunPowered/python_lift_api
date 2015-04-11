3f55b9108f29d4098903622d04c1edaaedc6c2c4
Tim van Boxtel <tim@vanboxtel.ca>
2015-04-11 10:35:56 -0400

    add num floors to BasePlan

--- plans.py
+++ plans.py
@@ -2,9 +2,10 @@
 
 
 class BasePlan(object):
-    name = None  # The plan name
-    n_els = 2    # The number of elevators
-    n_iter = 30  # The number of iterations
+    name = None    # The plan name
+    n_els = 2      # The number of elevators
+    n_iter = 30    # The number of iterations
+    n_floors = 10  # The number of floors
 
 
 class Training1(BasePlan):
