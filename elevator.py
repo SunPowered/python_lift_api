82b4bd5fee53fe5e8b9783101c1ba9d5c405c9d1
Tim van Boxtel <tim@vanboxtel.ca>
2015-04-11 09:07:07 -0400

    addded request_along_direction to Elevator.  Returns a sorted list of floors requested to the elevator along a given direction

--- elevator.py
+++ elevator.py
@@ -26,6 +26,22 @@ class Elevator(object):
         if self.is_request_assigned(floor, direction):
             self.requests.remove((floor, direction))
 
+    def requests_along_direction(self, direction):
+        """ Get a sorted list of requests along a given direction """
+        if direction != 1 and direction != -1:
+            raise ValueError("Improper direction, please enter +/-1")
+
+        def filter_direction(req):
+            return req[1] == direction
+
+        req = filter(filter_direction, self.requests)
+        floors = list(zip(*req)[0])
+        do_reverse = False
+        if direction == -1:
+            do_reverse = True
+        floors.sort(reverse=do_reverse)
+        return floors
+
     def is_button_pressed(self, btn):
         """ Simlpe check whether a button press is present """
         return btn in self.button_pressed
--- tests/test_elevator.py
+++ tests/test_elevator.py
@@ -19,10 +19,21 @@ class ElevatorTest(unittest.TestCase):
 
     def test_button_pressed(self):
         self.assertFalse(self.el.is_button_pressed(2))
-        self.el.buttons_pressed = [2, 5, 8]
+        self.el.button_pressed = [2, 5, 8]
         self.assertTrue(self.el.is_button_pressed(2))
         self.assertFalse(self.el.is_button_pressed(3))
 
+    def test_requests_along_direction(self):
+        self.assertRaises(ValueError, self.el.requests_along_direction, -2)
+
+        for floor, direction in [(3, -1), (1, 1), (7, 1), (8, -1), (1, -1)]:
+            self.el.assign_request(floor, direction)
+
+        floors = self.el.requests_along_direction(-1)
+        self.assertEqual(floors, [8, 3, 1])
+
+        floors = self.el.requests_along_direction(1)
+        self.assertEqual(floors, [1, 7])
 
 if __name__ == '__main__':
     unittest.main()
