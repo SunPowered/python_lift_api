31f219ed2d835966f5bcecf6eaa96673a029bde9
Tim van Boxtel <tim@vanboxtel.ca>
2015-04-13 15:03:30 -0400

    Added all plans

--- plan.py
+++ plan.py
@@ -17,3 +17,50 @@ class Training2(BasePlan):
     n_els = 4
     n_iter = 40
     n_floors = 20
+
+
+class Training3(Training2):
+    name = "training_3"
+    n_iter = 80
+
+
+class Random1(BasePlan):
+    name = "ch_rnd_500_1"
+    n_iter = 500
+    n_els = 4
+    n_floors = 25
+
+
+class Random2(Random1):
+    name = "ch_rnd_500_2"
+
+
+class Random3(Random1):
+    name = "ch_rnd_500_3"
+
+
+class Clustered1(Random1):
+    name = "ch_clu_500_1"
+
+
+class Clustered2(Clustered1):
+    name = "ch_clu_500_2"
+
+
+class Clustered3(Clustered1):
+    name = "ch_clu_500_3"
+
+
+class Realistic1(BasePlan):
+    name = "ch_rea_1000_1"
+    n_iter = 1000
+    n_els = 8
+    n_floors = 50
+
+
+class Realistic2(Realistic1):
+    name = "ch_rea_1000_2"
+
+
+class Realistic3(Realistic1):
+    name = "ch_rea_1000_3"
